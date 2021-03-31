9a2765c649b2af0ad884d6fc240edff27850daa2
Rajiv Kumbhakar <rajivnh@yahoo.com>
2021-03-31 11:00:48 +0530

    ll comiit

--- mvnw.cmd
+++ mvnw.cmd
@@ -33,6 +33,8 @@
 @REM MAVEN_SKIP_RC - flag to disable loading of mavenrc files
 @REM ----------------------------------------------------------------------------
 
+set JAVA_HOME=C:\jdk\orajdk11
+
 @REM Begin all REM lines with '@' in case MAVEN_BATCH_ECHO is 'on'
 @echo off
 @REM set title of command window
--- src/main/java/com/git/GitdemoApplication.java
+++ src/main/java/com/git/GitdemoApplication.java
@@ -1,12 +1,32 @@
 package com.git;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GitdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitdemoApplication.class, args);
+	}
+
+	@Bean 
+	public Supplier<String> supply() {
+		return () -> "Hello From RAJIV";
+	}
+	
+	@Bean 
+	public Consumer<String> consume() {
+		return (n) -> System.out.println(n);
+	}
+	
+	@Bean 
+	public Function<String, String> name() {
+		return (n) -> n + " k";
 	}
 }
